e83bd2e76887a84fc086e19c26ea5d0540500afb
Denis Zhukov <deniszhukov.hachiko@gmail.com>
2024-10-11 20:29:17 +0300

    fix(contacts): italic is removed

--- src/commands/contacts.ts
+++ src/commands/contacts.ts
@@ -4,7 +4,7 @@ import {Context} from "telegraf";
 const developer = '@Denis_Zhukov_Hachiko';
 
 export const contacts = async (ctx: Context)=>{
-    await ctx.replyWithMarkdownV2(`Разработчик:\n${developer}`)
+    await ctx.reply(`Разработчик:\n${developer}`)
 }
 
 bot.command('contacts', contacts)
