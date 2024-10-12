510003b6e700c4fd4b04f8a26bfdf989936e064f
Denis Zhukov <deniszhukov.hachiko@gmail.com>
2024-10-12 16:11:33 +0300

    feat(db): onDelete is created

--- prisma/migrations/20241012131107_on_delete_is_added/migration.sql
+++ prisma/migrations/20241012131107_on_delete_is_added/migration.sql
@@ -10,27 +10,8 @@ ALTER TABLE "class_call_schedule" DROP CONSTRAINT "class_call_schedule_class_id_
 -- DropForeignKey
 ALTER TABLE "leave_schedule" DROP CONSTRAINT "leave_schedule_scheduleId_fkey";
 
--- CreateTable
-CREATE TABLE "admins" (
-    "id" INTEGER NOT NULL,
-    "surname" TEXT,
-    "name" TEXT NOT NULL,
-    "username" TEXT,
-
-    CONSTRAINT "admins_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "admin_schedule" (
-    "id" SERIAL NOT NULL,
-    "day_of_week" "DayOfWeek" NOT NULL,
-    "admin_id" INTEGER NOT NULL,
-    "shift" "Shift" NOT NULL,
-    "start_time" TIMESTAMP(3) NOT NULL,
-    "end_time" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "admin_schedule_pkey" PRIMARY KEY ("id")
-);
+-- DropEnum
+DROP TYPE "Shift";
 
 -- AddForeignKey
 ALTER TABLE "canteen_schedule" ADD CONSTRAINT "canteen_schedule_scheduleId_fkey" FOREIGN KEY ("scheduleId") REFERENCES "schedule"("id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -43,6 +24,3 @@ ALTER TABLE "class_call_schedule" ADD CONSTRAINT "class_call_schedule_class_id_f
 
 -- AddForeignKey
 ALTER TABLE "class_call_schedule" ADD CONSTRAINT "class_call_schedule_call_schedule_id_fkey" FOREIGN KEY ("call_schedule_id") REFERENCES "lesson"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "admin_schedule" ADD CONSTRAINT "admin_schedule_admin_id_fkey" FOREIGN KEY ("admin_id") REFERENCES "admins"("id") ON DELETE CASCADE ON UPDATE CASCADE;
